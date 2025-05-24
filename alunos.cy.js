175e74176d57552f656905715deb0e224253109c
Bagrielzin <89031177+Bagrielzin@users.noreply.github.com>
2025-05-23 22:27:44 -0300

    Adicionando mais 3 testes de alunos

--- alunos.cy.js
+++ alunos.cy.js
@@ -3,8 +3,8 @@ const uuid = () => Cypress._.random(0, 1e6)
 const id = uuid()
 const testname = `testname${id}`
 
-describe('Testes de criação de usuário', () => {
-  it.skip('Teste de criar usuário com sucesso', () => {
+describe('Testes de criação de um usuário do tipo aluno', () => {
+  it('Teste de criar usuário com sucesso', () => {
     cy.visit('https://confianopai.com/login')
     cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
     cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
@@ -18,7 +18,7 @@ describe('Testes de criação de usuário', () => {
     cy.get('.Toastify__toast-body > :nth-child(2)').should("contain.text", "Usuário criado com sucesso!")
   })
   
-  it.skip('Teste de criar usuário com mesmo email', () => {
+  it('Teste de criar usuário com mesmo email', () => {
     cy.visit('https://confianopai.com/login')
     cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
     cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
@@ -32,7 +32,7 @@ describe('Testes de criação de usuário', () => {
     cy.get('.Toastify__toast-body > :nth-child(2)').should("contain.text", "Falha ao criar usuário.")
   })
   
-  it.skip('Teste de criar usuário com email invalido', () => {
+  it('Teste de criar usuário com email invalido', () => {
     cy.visit('https://confianopai.com/login')
     cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
     cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
@@ -44,5 +44,30 @@ describe('Testes de criação de usuário', () => {
     cy.get(':nth-child(3) > .sc-bqOYya > .sc-gHjVMF').type(testname)
     cy.get(':nth-child(4) > .sc-irLvIq > .sc-csKJxZ').click()
     cy.get('.Toastify__toast-body > :nth-child(2)').should("contain.text", "Por favor, insira um endereço de email válido.")
+  })
+
+  it('Teste de tentativa de login de um novo usuário', () => {
+    cy.visit('https://confianopai.com/login')
+    cy.get(':nth-child(2) > .sc-ktwOfi').type("joao@")
+    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+    cy.get('.sc-csKJxZ').click()
+    cy.url().should('eq', 'https://confianopai.com/aluno/projetos')
+  })
+
+  it('Teste de tentativa de login de um novo usuário com senha errada', () => {
+    cy.visit('https://confianopai.com/login')
+    cy.get(':nth-child(2) > .sc-ktwOfi').type("joao@")
+    cy.get(':nth-child(3) > .sc-ktwOfi').type("1234")
+    cy.get('.sc-csKJxZ').click()
+    cy.url().should('eq', 'https://confianopai.com/login')
+  })
+
+  it('Nome de usuário não é case sensetive (não difere letras maiusculas e minusculas', () => {
+    cy.visit('https://confianopai.com/login')
+    cy.get(':nth-child(2) > .sc-ktwOfi').type("JOAO@")
+    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+    cy.get('.sc-csKJxZ').click()
+    cy.wait(1000)
+    cy.url().should('eq', 'https://confianopai.com/login')
   })
 })
