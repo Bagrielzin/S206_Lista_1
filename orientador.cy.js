243b2654987745f277c143b4af4ba98f0eeccab2
Matheus Netto <125837414+MatheusNetto1@users.noreply.github.com>
2025-05-24 23:46:08 -0300

    cria funções úteis e tira os skips

--- orientador.cy.js
+++ orientador.cy.js
@@ -5,13 +5,9 @@ const testname = `testname${id}`
 
 describe('Testes de criação de usuário', () => {
 
-  it.skip('Teste de criar orientador com sucesso', () => {
-    cy.visit('https://confianopai.com/login')
-    cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
-    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
-    cy.get('.sc-csKJxZ').click()
-    cy.get('[href="/adm/novo-usuario"]').click()
-    cy.get('.sc-dsAqUS').select('Orientador')
+  it('Teste de criar orientador com sucesso', () => {
+    login()
+    seleciona_orientador()
     cy.get(':nth-child(1) > .sc-bqOYya > .sc-gHjVMF').type(testname)
     cy.get(':nth-child(2) > .sc-bqOYya > .sc-gHjVMF').type(testname + "@")
     cy.get(':nth-child(3) > .sc-bqOYya > .sc-gHjVMF').type(testname)
@@ -19,13 +15,9 @@ describe('Testes de criação de usuário', () => {
     cy.get('.Toastify__toast-body > :nth-child(2)').should("contain.text", "Usuário criado com sucesso!")
   })
 
-  it.skip('Teste de criar orientador já existente', () => {
-    cy.visit('https://confianopai.com/login')
-    cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
-    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
-    cy.get('.sc-csKJxZ').click()
-    cy.get('[href="/adm/novo-usuario"]').click()
-    cy.get('.sc-dsAqUS').select('Orientador')
+  it('Teste de criar orientador já existente', () => {
+    login()
+    seleciona_orientador()
     cy.get(':nth-child(1) > .sc-bqOYya > .sc-gHjVMF').type("testname782721")
     cy.get(':nth-child(2) > .sc-bqOYya > .sc-gHjVMF').type("testname782721@")
     cy.get(':nth-child(3) > .sc-bqOYya > .sc-gHjVMF').type("testname782721")
@@ -33,27 +25,9 @@ describe('Testes de criação de usuário', () => {
     cy.get('.Toastify__toast-body > :nth-child(2)').should("contain.text", "Falha ao criar usuário.")
   })
 
-  it.skip('Teste de criar orientador com email invalido', () => {
-    cy.visit('https://confianopai.com/login')
-    cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
-    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
-    cy.get('.sc-csKJxZ').click()
-    cy.get('[href="/adm/novo-usuario"]').click()
-    cy.get('.sc-dsAqUS').select('Orientador')
-    cy.get(':nth-child(1) > .sc-bqOYya > .sc-gHjVMF').type(testname)
-    cy.get(':nth-child(2) > .sc-bqOYya > .sc-gHjVMF').type(testname)
-    cy.get(':nth-child(3) > .sc-bqOYya > .sc-gHjVMF').type(testname)
-    cy.get(':nth-child(4) > .sc-irLvIq > .sc-csKJxZ').click()
-    cy.get('.Toastify__toast-body > :nth-child(2)').should("contain.text", "Por favor, insira um endereço de email válido.")
-  })
-
-  it.skip('Teste de criar orientador com email invalido', () => {
-    cy.visit('https://confianopai.com/login')
-    cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
-    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
-    cy.get('.sc-csKJxZ').click()
-    cy.get('[href="/adm/novo-usuario"]').click()
-    cy.get('.sc-dsAqUS').select('Orientador')
+  it('Teste de criar orientador com email invalido', () => {
+    login()
+    seleciona_orientador()
     cy.get(':nth-child(1) > .sc-bqOYya > .sc-gHjVMF').type(testname)
     cy.get(':nth-child(2) > .sc-bqOYya > .sc-gHjVMF').type(testname)
     cy.get(':nth-child(3) > .sc-bqOYya > .sc-gHjVMF').type(testname)
@@ -62,25 +36,16 @@ describe('Testes de criação de usuário', () => {
   })
 
   it('Teste de criar orientador com campos obrigatórios vazios', () => {
-    cy.visit('https://confianopai.com/login')
-    cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
-    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
-    cy.get('.sc-csKJxZ').click()
-    cy.get('[href="/adm/novo-usuario"]').click()
-    cy.get('.sc-dsAqUS').select('Orientador')
+    login()
+    seleciona_orientador()
     cy.get(':nth-child(4) > .sc-irLvIq > .sc-csKJxZ').click()
     cy.url().should('eq', 'https://confianopai.com/adm/novo-usuario')
     
   })
 
-
   it('Teste de criar orientador com senha curta demais', () => {
-    cy.visit('https://confianopai.com/login')
-    cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
-    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
-    cy.get('.sc-csKJxZ').click()
-    cy.get('[href="/adm/novo-usuario"]').click()
-    cy.get('.sc-dsAqUS').select('Orientador')
+    login()
+    seleciona_orientador()
     cy.get(':nth-child(1) > .sc-bqOYya > .sc-gHjVMF').type(testname)
     cy.get(':nth-child(2) > .sc-bqOYya > .sc-gHjVMF').type(`${testname}@email.com`)
     cy.get(':nth-child(3) > .sc-bqOYya > .sc-gHjVMF').type("12")
@@ -90,10 +55,8 @@ describe('Testes de criação de usuário', () => {
   })
 
   it('Teste de criar usuário sem selecionar tipo de usuário', () => {
-    cy.visit('https://confianopai.com/login')
-    cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
-    cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
-    cy.get('.sc-csKJxZ').click()
+    login()
+
     cy.get('[href="/adm/novo-usuario"]').click()
     cy.get('input[name="nome"]').should('not.exist')
     cy.get('input[name="email"]').should('not.exist')
@@ -103,3 +66,15 @@ describe('Testes de criação de usuário', () => {
 
   })
 })
+
+function login() {
+  cy.visit('https://confianopai.com/login')
+  cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
+  cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+  cy.get('.sc-csKJxZ').click()
+}
+
+function seleciona_orientador() {
+  cy.get('[href="/adm/novo-usuario"]').click()
+  cy.get('.sc-dsAqUS').select('Orientador')
+}
