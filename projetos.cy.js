635a52d7464a9cb11c20d3328b7401f3f2ee7309
Eduardo <160641722+odraude222k@users.noreply.github.com>
2025-05-28 12:42:41 -0300

    Projetos.cy.js

--- projetos.cy.js
+++ projetos.cy.js
@@ -51,3 +51,46 @@ describe('criando projeto', () => {
   })
 })
 
+describe('Teste de uma fase', ()=>{
+    it('Teste de uma fase com sucesso', () => {
+        cy.visit('https://confianopai.com/login')
+        cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
+        cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+        cy.get('.sc-csKJxZ').click()
+        cy.get('[href="/adm/fases"]').click()
+        cy.get('.sc-ia-dotI').click()
+        cy.get(':nth-child(1) > .sc-jnbAOD').type('Final')
+        cy.get(':nth-child(2) > .sc-jnbAOD').type('2025-06-09')
+        cy.get(':nth-child(3) > .sc-jnbAOD').type('6')
+        cy.get('.btn-primary').click()
+
+    })
+
+    it('Teste de uma fase com mesmo nome', () => {
+        cy.visit('https://confianopai.com/login')
+        cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
+        cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+        cy.get('.sc-csKJxZ').click()
+        cy.get('[href="/adm/fases"]').click()
+        cy.get('.sc-ia-dotI').click()
+        cy.get(':nth-child(1) > .sc-jnbAOD').type('Final')
+        cy.get(':nth-child(2) > .sc-jnbAOD').type('2025-09-09')
+        cy.get(':nth-child(3) > .sc-jnbAOD').type('2')
+        cy.get('.btn-primary').click()
+
+    })
+
+    it('Teste de uma fase com entregas = 0', () => {
+        cy.visit('https://confianopai.com/login')
+        cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
+        cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+        cy.get('.sc-csKJxZ').click()
+        cy.get('[href="/adm/fases"]').click()
+        cy.get('.sc-ia-dotI').click()
+        cy.get(':nth-child(1) > .sc-jnbAOD').type('Final')
+        cy.get(':nth-child(2) > .sc-jnbAOD').type('2025-07-07')
+        cy.get(':nth-child(3) > .sc-jnbAOD').type('0')
+        cy.get('.btn-primary').click()
+
+    })
+})
