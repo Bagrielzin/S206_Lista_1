536132c802abf1f24f15b89b425436b4bedeb122
Eduardo <160641722+odraude222k@users.noreply.github.com>
2025-05-28 16:36:10 -0300

    projetos.cy.js

--- projetos.cy.js
+++ projetos.cy.js
@@ -59,6 +59,20 @@ describe('Testes da parte de projetos', () => {
       cy.get('.btn-primary').click()
 
     })
+    
+    it('Teste de uma fase com mesmo nome', () => {
+        cy.visit('https://confianopai.com/login')
+        cy.get(':nth-child(2) > .sc-ktwOfi').type("matheusb@")
+        cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+        cy.get('.sc-csKJxZ').click()
+        cy.get('[href="/adm/fases"]').click()
+        cy.get('.sc-ia-dotI').click()
+        cy.get(':nth-child(1) > .sc-jnbAOD').type('Final')
+        cy.get(':nth-child(2) > .sc-jnbAOD').type('2025-09-09')
+        cy.get(':nth-child(3) > .sc-jnbAOD').type('2')
+        cy.get('.btn-primary').click()
+
+    })
 
   it('Teste de uma fase com entregas = 0', () => {
       cy.visit('https://confianopai.com/login')
